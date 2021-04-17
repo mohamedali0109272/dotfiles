2d5c877802c1976d3f03ffa5668c20b0678ceb46
mohamedali0109272 <mohamedali0109272@gmail.com>
2021-04-18 00:31:23 +0200

    dots

--- fish/config.fish
+++ fish/config.fish
@@ -1,12 +1,12 @@
 
 ### EXPORT ###
-#set fish_greeting  
-set EDITOR "vim"
+set fish_greeting  
+set EDITOR "nvim"
 
 
 ### SET EITHER DEFAULT EMACS MODE OR VI MODE ###
 function fish_user_key_bindings
-  #fish_default_key_bindings
+  # fish_default_key_bindings
   fish_vi_key_bindings
 end
 
@@ -18,14 +18,22 @@ end
 
 
 # Changing "ls" to "exa"
-#alias ls='exa -al --color=always --group-directories-first' # my preferred listing
-#alias la='exa -a --color=always --group-directories-first'  # all files and dirs
-#alias ll='exa -l --color=always --group-directories-first'  # long format
-#alias lt='exa -aT --color=always --group-directories-first' # tree listing
+#alias ls='exa  --color=always --group-directories-first' # my preferred listing
+alias la='exa -ah --color=always --group-directories-first'  # all files and dirs
+alias ll='exa -lah --color=always --group-directories-first'  # long format
+#alias lt='exa -aTh --color=always --group-directories-first' # tree listing
 
 
 # vim 
 alias vim='nvim'
+
+#cat
+alias ca='bat'
+
+#pacman
+alias pac='sudo pacman'
+alias pacu='sudo pacman -Syyu --noconfirm'
+alias pacw='sudo pacman -Syyuw --noconfirm'
 
 # Colorize grep output (good for log files)
 alias grep='grep --color=auto'
@@ -62,3 +70,6 @@ alias pi="ping 1.1.1.1"
 
 ### SETTING THE STARSHIP PROMPT ###
 starship init fish | source
+
+# Created by `pipx` on 2021-04-13 10:55:56
+set PATH $PATH /home/memo/.local/bin
